bac302f2e1f7e5ac5fd7a373e378c391979d8d5f
jempurich <jempurich@gmail.com>
2020-04-15 23:18:25 +0800

    update meta tags

--- src/components/PostContent.tsx
+++ src/components/PostContent.tsx
@@ -234,7 +234,7 @@ export const PostFullContent = styled.section`
 
   h1 {
     margin: 0.5em 0 0.2em 0;
-    font-size: 4.6rem;
+    font-size: 4rem;
     font-weight: 700;
   }
   @media (max-width: 500px) {
